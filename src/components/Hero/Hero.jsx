83d46251d069bd023f1ab2eae6334b63735eceff
Rocío Castillo <rociocastillo.s@gmail.com>
2025-08-01 13:22:50 -0400

    Actualización secciín Hero, se agregó imagen

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { motion } from "framer-motion"
 import { slipeUp } from '../../utility/animation'
 import fondo from '../../assets/fondo.png';
+import saludo from '../../assets/saludo.png'
 
 
 const Hero = () => {
@@ -35,24 +36,34 @@ const Hero = () => {
       ></div>
 
       {/* Contenido sobre la imagen */}
-      <div className="w-full max-w-6xl flex items-center justify-center z-10">
-        <div className="grid grid-cols-1 gap-10 items-center w-full">
-          <div className="p-6 sm:p-10 text-center flex flex-col justify-center items-center bg-zinc-100 rounded-xl border border-black border-b-[4px]">
-            <h1 className="text-4xl sm:text-9xl font-extrabold text-zinc-700 mb-6 mt-6">
-              Rocío Castillo
-            </h1>
-            <p className="text-zinc-700 text-base sm:text-4xl max-w-prose font-bold">
-              Desarrolladora y Diseñadora web
-            </p>
-            <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
-              <a href="#proyectos"><button className="cursor-pointer
-             rounded-3xl px-6 py-2 bg-gradient-to-r from-indigo-200 to-indigo-400 text-zinc-700 font-bold sm:text-xl">
-                Ver Proyectos
-              </button></a>
-            </div>
-          </div>
-        </div>
+    <div className="relative w-full max-w-6xl flex items-center justify-center">
+  {/* Imagen saludo, centrada y detrás del recuadro */}
+  <img
+    src={saludo}
+    alt="Decoración"
+    className="absolute top-4 sm:top-8 left-1/2 -translate-x-1/2 w-48 sm:w-72 "
+  />
+
+  {/* Recuadro principal */}
+  <div className="grid grid-cols-1 gap-10 items-center w-full z-10 mt-24 sm:mt-40 lg:mt-48">
+    <div className="mt-36 p-6 sm:p-10 text-center flex flex-col justify-center items-center bg-zinc-100 rounded-xl border border-black border-b-[4px]">
+      <h1 className="text-4xl sm:text-9xl font-extrabold text-zinc-700 mb-6 mt-6">
+        Rocío Castillo
+      </h1>
+      <p className="text-zinc-700 text-base sm:text-4xl max-w-prose font-bold">
+        Desarrolladora y Diseñadora web
+      </p>
+      <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
+        <a href="#proyectos">
+          <button className="cursor-pointer rounded-3xl px-6 py-2 bg-gradient-to-r from-indigo-200 to-indigo-400 text-zinc-700 font-bold sm:text-xl">
+            Ver Proyectos
+          </button>
+        </a>
       </div>
+    </div>
+  </div>
+</div>
+
     </section>
     
   )
